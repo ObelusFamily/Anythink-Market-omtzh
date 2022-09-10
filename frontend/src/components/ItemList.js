414e8d9e54ddf9f4fc09fe2ad56d5811a33a28c5
galnir <nirnir122@gmail.com>
2022-09-10 19:43:10 +0300

    test

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -4,19 +4,22 @@ import React from "react";
 
 const ItemList = (props) => {
   if (!props.items) {
-    return <div className="py-4">Loading...</div>;
+    return (
+      <div className="py-4" id="empty">
+        Loading...
+      </div>
+    );
   }
 
-  if (props.items?.length === 0) {
+  if (props.items.length === 0) {
     if (props.title?.length) {
       return (
         <div id="empty" className="py-4 no-items">
           No items found for "<strong>{props.title}</strong>"
         </div>
       );
-    } else if (!props.title || props.title === "") {
-      return <div className="py-4 no-items">No items are here... yet.</div>;
     }
+    return <div className="py-4 no-items">No items are here... yet.</div>;
   }
 
   return (
