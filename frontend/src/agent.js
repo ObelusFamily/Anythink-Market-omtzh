1c975ee4379403cc2ac1898223bfea64f97939d1
galnir <nirnir122@gmail.com>
2022-09-12 12:37:44 +0300

    place correct backend url

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -6,7 +6,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 const BACKEND_URL =
   process.env.NODE_ENV !== "production"
     ? process.env.REACT_APP_BACKEND_URL
-    : "https://api.anythink.market";
+    : "https://anythink-market-omtzh-api.herokuapp.com/";
 
 const API_ROOT = `${BACKEND_URL}/api`;
 
